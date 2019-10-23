7d2944e0b9e6fa56aa24f65171a4abc9fb357c47
BrianMwangi21 <mwangibrian8@gmail.com>
2019-10-23 21:17:12 +0300

    Finished Netflix Feign

--- src/main/java/com/kabiru/alpha/feign/NetflixFeign.java
+++ src/main/java/com/kabiru/alpha/feign/NetflixFeign.java
@@ -9,27 +9,46 @@ import java.util.List;
 @FeignClient(name="netflixFeign", url="http://127.0.0.1:8000/", configuration = FeignConfig.class)
 public interface NetflixFeign {
 
+    // Get all users
+    @RequestMapping( method = RequestMethod.GET, value = "users" )
+    List<User> getAllUsers();
+
+    // Get specific user
+    @RequestMapping( method = RequestMethod.GET, value = "users/{id}" )
+    User getSpecificUser(@PathVariable( name = "id" ) long id);
+
     // Create new user
-    @RequestMapping( method = RequestMethod.POST, value = "create-user" )
+    @RequestMapping( method = RequestMethod.POST, value = "users" )
     User createUser(@RequestBody User user);
 
-    // Suggest new movie
-    @RequestMapping( method = RequestMethod.POST, value = "suggest-movie" )
-    Movie suggestMovie(@RequestBody Movie movie);
+    // Get all categories
+    @RequestMapping( method = RequestMethod.GET, value = "categories" )
+    List<Category> getAllCategories();
+
+    // Get specific category
+    @RequestMapping( method = RequestMethod.GET, value = "categories/{id}" )
+    Category getSpecificCategory(@PathVariable( name = "id" ) long id);
+
+    // Create new category
+    @RequestMapping( method = RequestMethod.POST, value = "categories" )
+    Category createCategory(@RequestBody Category category);
 
     // Get list of movies
-    @RequestMapping( method = RequestMethod.GET, value = "all-movies" )
+    @RequestMapping( method = RequestMethod.GET, value = "movies" )
     List<Movie> getAllMovies();
 
     // Get specific movie
-    @RequestMapping( method = RequestMethod.GET, value = "get-movie/{category-id}" )
+    @RequestMapping( method = RequestMethod.GET, value = "movies/categories/{category-id}" )
     Movie getSpecificMovie(@PathVariable( name = "category-id" ) long id, @RequestParam( name = "type") String type);
 
-    // Update specific movie
-    @RequestMapping( method = RequestMethod.GET, value = "update-movie/{id}" )
-    Movie updateSpecificMovie(@PathVariable( name = "id" ) long id, @RequestBody Movie movie, @RequestBody User user);
+    // Suggest new movie
+    @RequestMapping( method = RequestMethod.POST, value = "movies/suggest" )
+    Movie suggestMovie(@RequestBody Movie movie);
 
-    // Update specific movie
-    @RequestMapping( method = RequestMethod.GET, value = "update-movie/{id}" )
-    String deleteSpecificMovie(@PathVariable( name = "id" ) long id, @RequestBody Movie movie, @RequestBody User user);
+    // Get specific movie
+    @RequestMapping( method = RequestMethod.GET, value = "movies/{id}" )
+    Movie getSpecificMovie(@PathVariable( name = "id" ) long id);
+
+    @RequestMapping( method = RequestMethod.GET, value = "movies/{id}" )
+    String deleteSpecificMovie(@PathVariable( name = "id" ) long id, @RequestParam( name = "user") long user_id);
 }
